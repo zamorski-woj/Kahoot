15e7f9a1f562a3cb74ad5fa57b35044c1d0be4b0
zamorski-woj <zamorski.woj@gmail.com>
2024-04-18 12:24:59 +0200

    cleaner namespace, described tests

--- Kahoot/Trie.cs
+++ Kahoot/Trie.cs
@@ -1,99 +1,98 @@
-﻿namespace Kahoot
+﻿namespace Kahoot;
+
+public class Trie
 {
-    public class Trie
+    public class Node
     {
-        public class Node
-        {
-            public Dictionary<char, Node> children;
-            public bool isWord;
+        public Dictionary<char, Node> children;
+        public bool isWord;
 
-            public Node()
+        public Node()
+        {
+            children = [];
+            isWord = false;
+        }
+    }
+
+    private readonly Node root;
+
+    public Trie()
+    {
+        root = new Node();
+    }
+
+    public Trie(List<string> list)
+    {
+        root = new Node();
+        foreach (string word in list)
+        {
+            this.Add(word);
+        }
+    }
+
+    public void Add(string word)
+    {
+        Node currentNode = root;
+        foreach (char letter in word)
+        {
+            if (!currentNode.children.TryGetValue(letter, out Node? value))
             {
-                children = [];
-                isWord = false;
+                Node node = new();
+                value = node;
+                currentNode.children.Add(letter, value);
+            }
+            currentNode = value;//move down along the branch
+        }
+        currentNode.isWord = true;
+    }
+
+    public bool Contains(string word)
+    {
+        Node current = root;
+        foreach (char letter in word)
+        {
+            if (current.children.TryGetValue(letter, out Node? value))
+            {
+                current = value;
+            }
+            else
+            {
+                return false;
             }
         }
+        return current.isWord;//if it is not considered a word, do not confirm
+    }
 
-        private readonly Node root;
+    public List<string> StartsWith(string query)
+    {
+        List<string> completedWords = [];
 
-        public Trie()
+        Node current = root;
+        foreach (char letter in query)
         {
-            root = new Node();
-        }
-
-        public Trie(List<string> list)
-        {
-            root = new Node();
-            foreach (string word in list)
+            if (current.children.TryGetValue(letter, out Node? value))
             {
-                this.Add(word);
+                current = value;
+            }
+            else
+            {
+                return completedWords;
             }
         }
+        Autocomplete(current, query, completedWords);
+        return completedWords;
+    }
 
-        public void Add(string word)
+    private static void Autocomplete(Node current, string query, List<string> completedWords)
+    {
+        if (current.isWord)
         {
-            Node currentNode = root;
-            foreach (char letter in word)
-            {
-                if (!currentNode.children.TryGetValue(letter, out Node? value))
-                {
-                    Node node = new();
-                    value = node;
-                    currentNode.children.Add(letter, value);
-                }
-                currentNode = value;//move down along the branch
-            }
-            currentNode.isWord = true;
+            completedWords.Add(query);
         }
 
-        public bool Contains(string word)
+        foreach (char key in current.children.Keys)
         {
-            Node current = root;
-            foreach (char letter in word)
-            {
-                if (current.children.TryGetValue(letter, out Node? value))
-                {
-                    current = value;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return current.isWord;//if it is not considered a word, do not confirm
-        }
-
-        public List<string> StartsWith(string query)
-        {
-            List<string> completedWords = [];
-
-            Node current = root;
-            foreach (char letter in query)
-            {
-                if (current.children.TryGetValue(letter, out Node? value))
-                {
-                    current = value;
-                }
-                else
-                {
-                    return completedWords;
-                }
-            }
-            Autocomplete(current, query, completedWords);
-            return completedWords;
-        }
-
-        private static void Autocomplete(Node current, string query, List<string> completedWords)
-        {
-            if (current.isWord)
-            {
-                completedWords.Add(query);
-            }
-
-            foreach (char key in current.children.Keys)
-            {
-                Autocomplete(current.children[key], query + key, completedWords);//recursively search for every match
-            }
+            Autocomplete(current.children[key], query + key, completedWords);//recursively search for every match
         }
     }
 }
--- KahootTest/Tests.cs
+++ KahootTest/Tests.cs
@@ -1,60 +1,72 @@
 using FluentAssertions;
 using Kahoot;
 
-namespace KahootTest
+namespace KahootTest;
+
+public class Tests
 {
-    public class Tests
+    //Arrange
+    private readonly Trie dictionary = new(["car", "carpet", "java", "javascript", "internet"]);
+
+    [Fact]  // a) After user types “c”, the function should return a list of two entries: “car” and “carpet”.
+    public void TestA()
     {
-        private readonly Trie dictionary = new(["car", "carpet", "java", "javascript", "internet"]);
+        //Act
+        List<string> candidates = dictionary.StartsWith("c");
+        //Assert
+        candidates.Count.Should().Be(2);
+        candidates.Should().Contain("car");
+        candidates.Should().Contain("carpet");
+    }
 
-        [Fact]  // a) After user types “c”, the function should return a list of two entries: “car” and “carpet”.
-        public void TestA()
-        {
-            List<string> candidates = dictionary.StartsWith("c");
-            candidates.Count.Should().Be(2);
-            candidates.Should().Contain("car");
-            candidates.Should().Contain("carpet");
-        }
+    [Fact]  // b) After user types “car”, the function should still return the same list of entries as above, that is “car” and “carpet”. Please note that “car” is still included.
+    public void TestB()
+    {
+        //Act
+        List<string> candidates = dictionary.StartsWith("car");
+        //Assert
+        candidates.Count.Should().Be(2);
+        candidates.Should().Contain("car");
+        candidates.Should().Contain("carpet");
+    }
 
-        [Fact]  // b) After user types “car”, the function should still return the same list of entries as above, that is “car” and “carpet”. Please note that “car” is still included.
-        public void TestB()
-        {
-            List<string> candidates = dictionary.StartsWith("car");
-            candidates.Count.Should().Be(2);
-            candidates.Should().Contain("car");
-            candidates.Should().Contain("carpet");
-        }
+    [Fact]  // c) After user types “carp”, the function should return a list of one entry: “carpet”.
+    public void TestC()
+    {
+        //Act
+        List<string> candidates = dictionary.StartsWith("carp");
+        //Assert
+        candidates.Count.Should().Be(1);
+        candidates.Should().Contain("carpet");
+    }
 
-        [Fact]  // c) After user types “carp”, the function should return a list of one entry: “carpet”.
-        public void TestC()
-        {
-            List<string> candidates = dictionary.StartsWith("carp");
-            candidates.Count.Should().Be(1);
-            candidates.Should().Contain("carpet");
-        }
+    [Fact] //d) After user types “jav”, the function should return a list of two entries: “java” and “javascript”.
+    public void TestD()
+    {
+        //Act
+        List<string> candidates = dictionary.StartsWith("jav");
+        //Assert
+        candidates.Count.Should().Be(2);
+        candidates.Should().Contain("java");
+        candidates.Should().Contain("javascript");
+    }
 
-        [Fact] //d) After user types “jav”, the function should return a list of two entries: “java” and “javascript”.
-        public void TestD()
-        {
-            List<string> candidates = dictionary.StartsWith("jav");
-            candidates.Count.Should().Be(2);
-            candidates.Should().Contain("java");
-            candidates.Should().Contain("javascript");
-        }
+    [Fact]  // e) After user types “intern”, the function should return a list of one entry: “internet”.
+    public void TestE()
+    {
+        //Act
+        List<string> candidates = dictionary.StartsWith("intern");
+        //Assert
+        candidates.Count.Should().Be(1);
+        candidates.Should().Contain("internet");
+    }
 
-        [Fact]  // e) After user types “intern”, the function should return a list of one entry: “internet”.
-        public void TestE()
-        {
-            List<string> candidates = dictionary.StartsWith("intern");
-            candidates.Count.Should().Be(1);
-            candidates.Should().Contain("internet");
-        }
-
-        [Fact]  // f) After user types “foo”, the function should return an empty list, since there is not a single word in the dictionary we’re working on that starts with “foo”.
-        public void TestF()
-        {
-            List<string> candidates = dictionary.StartsWith("foo");
-            candidates.Count.Should().Be(0);
-        }
+    [Fact]  // f) After user types “foo”, the function should return an empty list, since there is not a single word in the dictionary we’re working on that starts with “foo”.
+    public void TestF()
+    {
+        //Act
+        List<string> candidates = dictionary.StartsWith("foo");
+        //Assert
+        candidates.Count.Should().Be(0);
     }
 }
